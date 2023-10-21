5508ef35ae5fb095b6f41f4961a5b7607ba06fd7
IngridKa <139657585+IngridKamdem@users.noreply.github.com>
2023-10-21 19:23:18 +0200

    Update GeoInfo.jsx

--- my react-vite project/src/components/GeoInfo.jsx
+++ my react-vite project/src/components/GeoInfo.jsx
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
-function GeoInfo({ ipAddress }) {
+GeoInfo.propTypes = {
+  countryCode: PropTypes.string.isRequired,
+};
+
+
+function GeoInfo({ countryCode }) {
   const [geoData, setGeoData] = useState(null);
 
   useEffect(() => {
-    if (ipAddress) {
-      axios.get(`https://ipinfo.io/${ipAddress}/json`)
+      axios.get(`https://restcountries.com/v3.1/alpha/${countryCode}?fields=name,flags,flag`)
         .then((response) => {
           setGeoData(response.data);
         })
         .catch((error) => {
           console.error('Error fetching geolocation data:', error);
         });
-    }
-  }, [ipAddress]);
+  }, []);
 
   return (
     <div>
       <h2>Geographic and Time Information:</h2>
       {geoData && (
         <div>
-          <p>IP Address: {geoData.ipAddress}</p>
-          <p>City: {geoData.city}</p>
+
+          <img src={geoData.flags.svg} />
+          {/* <p>City: {geoData.city}</p>
           <p>Region: {geoData.region}</p>
           <p>Country: {geoData.country}</p>
-          <p>Timezone: {geoData.timezone}</p>
+          <p>Timezone: {geoData.timezone}</p> */}
         </div>
       )}
     </div>

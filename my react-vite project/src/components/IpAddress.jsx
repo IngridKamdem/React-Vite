61187ace584fc8be11b28104ae7526fd9fcc52da
IngridKa <139657585+IngridKamdem@users.noreply.github.com>
2023-10-21 19:23:55 +0200

    Update IpAddress.jsx

--- my react-vite project/src/components/IpAddress.jsx
+++ my react-vite project/src/components/IpAddress.jsx
@@ -1,31 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import '../App.css';
+import PropTypes from 'prop-types';
 
-const api =
-    "https://geo.ipify.org/api/v2/country?apiKey=at_YmyDr9fXPvZ98nmmc9Wg5HezwUENh";
-
-function App() {
-    const {ipAddress, setIpAddress} = useState(null)
-} 
-    
-    useEffect(() => {
-        fetch(api)
-        .then((response) => response.json())
-        .then((data) => {
-            setIpAddress(data.ip);
-        })
-        })
-        .catch((error) => {
-            console.error('Error when calling IP address:', error);
-        });
- []; 
-
-return (
-    <div>
-        <h2>Your IP Address:</h2>
-        <p></p>
-    </div>
+function IpAddress({ ipAddress }) {
+    return (
+        <div>
+            <h2>Your IP Address:</h2>
+            <p>{ipAddress}</p>
+        </div>
     );
+}
 
+IpAddress.propTypes = {
+    ipAddress: PropTypes.string.isRequired,
+};
 
 export default IpAddress;
